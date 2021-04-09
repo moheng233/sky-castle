45e7b0ab156e9f639d54c330d64968c4611361dc
moheng233 <ylm200322400@163.com>
2021-04-09 18:24:23 +0800

    整理架构

--- packages/wings/test/test.ts
+++
@@ -1,9 +0,0 @@
-import { WingsModule } from "../src/common";
-import { WingsServer } from "../src/server";
-
-const server = new WingsServer();
-
-@server.module("test")
-class TestModule extends WingsModule {
-
-}
